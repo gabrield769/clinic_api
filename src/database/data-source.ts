debffcfe552691af2cd72fa3c1033a0fb8c6f5d4
gabrield769 <gabrield7699@gmail.com>
2025-06-16 00:05:15 -0300

    fix: Adjust data source for production environment

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,22 +1,27 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import * as dotenv from 'dotenv';
-import { Patient } from "../entities/Patient";
-import { Doctor } from "../entities/Doctor";
-import { Appointment } from "../entities/Appointment";
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
+    
+    url: process.env.DATABASE_URL,
+    
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
+
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    synchronize: true,
+
+    synchronize: false, 
     logging: false,
-    entities: [Patient, Doctor, Appointment],
+
+    
+    entities: [__dirname + '/../entities/*.{js,ts}'],
     migrations: [],
     subscribers: [],
 });
